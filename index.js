390f6dbee243f8c62ece9d7ea85736a710e47880
caleb <kalebntoyian2004@gmail.com>
2024-07-23 14:52:33 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+// index.js
+
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function wrapAdjective
+  function wrapAdjective(highlight = "*") {
+    return function(adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+  
+  // Export functions for testing
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
